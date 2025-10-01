cc00da417c3bd8b1ec5c1dd4c6d44f66c93dc0d1
rahul <singhrahul38202@gmail.com>
2025-10-02 00:22:21 +0530

    add numRescueBoats.java

---
+++ Home Assignment 5/numRescueBoats.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] arr, int limit) {
+        Arrays.sort(arr);
+        int i =0;
+        int j = arr.length-1;
+        int count  = 0;
+        while(i<=j){
+            if(arr[j]==limit)
+            count++;
+            else if(arr[j]+arr[i]<=limit){
+                count++;
+                i++;
+            }
+            else count++;
+            j--;
+        }
+        return count;
+    }
+}
